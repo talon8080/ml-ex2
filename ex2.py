f7b64dcc851e2aa54ab32f9dcb82e58ce06c6c5d
orzinger <orzinger.job@gmail.com>
2019-05-15 19:36:19 +0300

    or1

--- ex2.py
+++ ex2.py
@@ -1,57 +1,36 @@
 import numpy as np
 from sklearn.utils import shuffle
 from scipy import stats
-import sys
 
 
-def prepareData(train_x_file_name,train_y_file_name,test_file_name):
-    arr = np.loadtxt(train_x_file_name, usecols=(1,2,3,4,5,6,7), dtype=np.float32, delimiter=',')
+def prepareData():
+    arr = np.loadtxt("train_x.txt", usecols=(1,2,3,4,5,6,7), dtype=np.float32, delimiter=',')
     df = list()
-    with open(train_x_file_name,'r') as file:
+    with open("train_x.txt",'r') as file:
         for i,line in enumerate(file):
             if line[0] == 'M':
                 appendcol = [1.,0.,0.]        
             elif line[0] == 'F':
                 appendcol = [0.,1.,0.]        
-            else: # 'I'
+            elif line[0] == 'I':
                 appendcol = [0.,0.,1.]
             df.append(np.append(arr[i],appendcol))
     x_train = np.array(df).astype('float32')
-    y_train = np.loadtxt(train_y_file_name)
+    y_train = np.loadtxt("train_y.txt")
     return x_train, y_train
 
 def normalizeData(x_train):
-    x_train = stats.zscore(x_train)
+    #x_train = stats.zscore(x_train)
+    m, d = x_train.shape
+    for i in range(d):
+        cmean = x_train[:,i].mean()
+        cstd = x_train[:,i].std()
+        if cstd == 0:
+            x_train[:,i] = 1.0 / float(m)
+        else:
+            x_train[:,i] = (x_train[:,i] - cmean) / cstd
     return x_train
-    ### Data normalization
-    # for i, column in enumerate(x_train.T):
-    #     cmean = column.mean()
-    #     cstd = column.std()
-    #     column = (column - cmean) / cstd
-    #     x_train[:,i] = column
 
-    #w = np.random.random((3,len(x_train[0])))
-
-def min_max_scaling(X):
-    n_samples = X.shape[0]
-    n_features = X.shape[1]
-
-    for i in range(n_features):
-        V = X[:,i]
-        V_min = V.min()
-        V_max = V.max()
-
-        if V_max != V_min:
-            V_tag = np.true_divide(V-V_min,V_max-V_min)
-            V_tag_min = V_tag.min()
-            V_tag_max = V_tag.max()
-            V_tag *= (V_tag_max-V_tag_min)
-            V_tag+= V_tag_min
-            X[:,i] = V_tag
-        else: # V_max == V_min:
-            X[:, i] = np.true_divide(1,n_samples)
-
-    return X
 
 def splitData(x_train, y_train):
     
@@ -76,59 +55,70 @@ def perceptron_fit(x_train, y_train):
     return w
     
 
-
-
-
-def perceptron_predict(w_perceptron, x_test, y_test):
-
-    labels = list()
-    e_perceptron = 0
-    c_perceptron = 0
-    for t in range(0,len(x_test)):
-        y_hat = np.argmax(np.dot(w_perceptron,x_test[t]))
-        labels.append(y_hat)
-        if y_hat != y_test[t]:
-            e_perceptron = e_perceptron + 1
-        else:
-            c_perceptron = c_perceptron + 1
-    print("Perceptron error: {}".format(float(e_perceptron)/len(x_test)))
-    print("Perceptron correct: {}".format(float(c_perceptron)/len(x_test)))
-
-
-
-
 def svm_fit(x_train, y_train):
     w = np.random.uniform(-0.5,0.5,(3,len(x_train[0])))
     alpha = np.random.random()
     eta = np.random.uniform(0.001,0.5)
+    svm_w_list = []
     for epoch in range(10):
         x_train, y_train = shuffle(x_train,y_train,random_state = 1) 
         for x,y in zip(x_train,y_train):
             y_hat = np.argmax(np.dot(w,x))
-            #loss = max([0.0,1.0 - np.dot(w[int(y),:],x) + np.dot(w[int(y_hat),:],x)])
-            #if loss > 0.0:
             if y_hat != y:
                 w[int(y),:] = (1-eta*alpha)*w[int(y),:] + eta*x
                 w[int(y_hat),:] = (1-eta*alpha)*w[int(y_hat),:] - eta*x
             for i in range(w.shape[0]):
-                if i!=y and i!=y_hat:
+                if i != y and i !=y_hat:
                     w[i,:] = (1-eta*alpha)*w[i,:]
-    return w
+        svm_w_list.append(w)
+    retrun svm_w_list
+
+
+def validate(w_list, x_valid, y_valid):
+
+    w_dict = {}
+    for w in w_list:
+        w_dict[w] = 0
+        for t in range(0,len(x_valid)):
+            y_hat = np.argmax(np.dot(w,x_valid[t]))
+            if y_hat == y_valid[t]:
+                w_dict[w] = w_dict[w] + 1
+    return max(w_dict.items(), key=operator.itemgetter(1))[0]
 
 def svm_predict(w_svm, x_test, y_test):
 
-    labels = list()
     e_perceptron = 0
     c_perceptron = 0
     for t in range(0,len(x_test)):
         y_hat = np.argmax(np.dot(w_svm,x_test[t]))
-        labels.append(y_hat)
         if y_hat != y_test[t]:
             e_perceptron = e_perceptron + 1
         else:
             c_perceptron = c_perceptron + 1
-    print("SVM error: {}".format(float(e_perceptron)/len(x_test)))
-    print("SVM correct: {}".format(float(c_perceptron)/len(x_test)))
+    print("svm error: {}".format(float(e_perceptron)/len(x_test)))
+    print("svm correct: {}".format(float(c_perceptron)/len(x_test)))
+ 
+	
+def min_max_scaling(X):
+    n_samples = X.shape[0]
+    n_features = X.shape[1]
+
+    for i in range(n_features):
+        V = X[:,i]
+        V_min = V.min()
+        V_max = V.max()
+
+        if V_max != V_min:
+            V_tag = np.true_divide(V-V_min,V_max-V_min)
+            V_tag_min = V_tag.min()
+            V_tag_max = V_tag.max()
+            V_tag *= (V_tag_max-V_tag_min)
+            V_tag+= V_tag_min
+            X[:,i] = V_tag
+        else: # V_max == V_min:
+            X[:, i] = np.true_divide(1,n_samples)
+
+    return X
 
 if __name__ == "__main__":
 
